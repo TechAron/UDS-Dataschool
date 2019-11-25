fc3ee6b53456e96991d0c4f32a4b9386f159461f
bdiender <57986970+bdiender@users.noreply.github.com>
2019-11-25 12:20:02 +0100

    Update relevantedocumenten.R

--- relevantedocumenten.R
+++ relevantedocumenten.R
@@ -12,10 +12,13 @@ tok <- tokens(september_corpus,         #tokeniseert door:
 
 #Woordenlijst thema migratie
 migr_tok <- tokens_select(tok,c("vluchte*","rohingya","asiel*","migr*","opvang","azc","integratie","grens"), selection = "keep", padding = FALSE)
-newthing <- ntoken(migr_tok)
-vluchtelingen_bool <- newthing > 0L
+newthing <- ntoken(migr_tok) #telt het aantal voorkomens van één van de woorden op de woordenlijst per artikel
+vluchtelingen_bool <- newthing > 0L #als er meer dan 0 relevante woorden in voorkomen, is het een relevant artikel
+                                    #in vluchtelingen_bool komt voor elk artikel te staan of dit het geval is
 
-sept_C <- september_corpus
-docvars(sept_C, "vluchtelingen") <- vluchtelingen_bool
+sept_C <- september_corpus #slaat het corpus opnieuw voor het geval we het corpus per ongeluk slopen
 
-vlucht_sept <- corpus_subset(sept_C, vluchtelingen == TRUE)
+docvars(sept_C, "vluchtelingen") <- vluchtelingen_bool #maakt een nieuwe kolom waarin staat of het artikel relevant
+                                                       #is voor het onderwerp 'vluchtelingen'
+
+vlucht_sept <- corpus_subset(sept_C, vluchtelingen == TRUE) #maakt een nieuw corpus 'vlucht_sept' met alle relevante artikelen
