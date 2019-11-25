ca4503d36a297fa0ecc43c7b2699327dd7ec18f3
bdiender <57986970+bdiender@users.noreply.github.com>
2019-11-25 12:02:32 +0100

    Add files via upload

---
+++ relevantedocumenten.R
@@ -0,0 +1,21 @@
+library(quanteda)     #Voor tokensiatie, corpora, etc.
+library(topicmodels)  #Voor de topicmodels
+
+september_corpus <- corpus(september)   #Corpus van de data van september (was miss al)
+tok <- tokens(september_corpus,         #tokeniseert door:
+              remove_numbers = TRUE,        #nummers te verwijderen
+              remove_url = TRUE,            #urls te verwijderen
+              remove_symbols = TRUE,        #symbolen(?) te verwijderen
+              remove_punct = TRUE) %>%      #leestekens te verwijderen
+  tokens_tolower() %>%                      #alle letters kleine letters te maken
+  tokens_remove(stopwords::stopwords(language = "nl")) #en stopwoorden te verwijderen
+
+#Woordenlijst thema migratie
+migr_tok <- tokens_select(tok,c("vluchte*","rohingya","asiel*","migr*","opvang","azc","integratie","grens"), selection = "keep", padding = FALSE)
+newthing <- ntoken(migr_tok)
+vluchtelingen_bool <- newthing > 0L
+
+sept_C <- september_corpus
+docvars(sept_C, "vluchtelingen") <- vluchtelingen_bool
+
+vlucht_sept <- corpus_subset(sept_C, vluchtelingen == TRUE)
