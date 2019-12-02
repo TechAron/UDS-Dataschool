8bc217ae42d3703f9c0a8f19ae26972efba37cf7
TechAron <57986701+TechAron@users.noreply.github.com>
2019-12-02 10:34:13 +0100

    Update EUSubsetCode.R

--- EUSubsetCode.R
+++ EUSubsetCode.R
@@ -11,8 +11,8 @@ tok <- tokens(Newscorpus,         #tokeniseert door:
   tokens_tolower() %>%                      #alle letters kleine letters te maken
   tokens_remove(stopwords::stopwords(language = "nl"))  #en stopwoorden te verwijderen
 #Woordenlijst thema migratie
-EU_tok <- tokens_select(tok,c("EU", "euroscepticus", "juncker", "brexit", "nexit", "^e(u|urope(es|se))$", "boris_johnson", "^may$"), selection = "keep", padding = FALSE) #hij is een beetje aangepast. HIerdoor werd de LDA relevanter, met vaker dingen over advocaten, rechters, stoornissen et cetera. Psychiaters en ontsnappingen zijn eruit. 
-newthingEU <- ntoken(EU_tok) #telt het aantal voorkomens van één van de woorden op de woordenlijst per artikel
+EU_tok <- tokens_select(tok,c("euroscepticus", "juncker", "brexit", "nexit", "^e(u|urope(es|se))$", "boris_johnson", "^may$"), selection = "keep", padding = FALSE) #hij is een beetje aangepast. HIerdoor werd de LDA relevanter, met vaker dingen over advocaten, rechters, stoornissen et cetera. Psychiaters en ontsnappingen zijn eruit. 
+newthingEU <- ntoken(EU_tok) #telt het aantal voorkomens van Ã©Ã©n van de woorden op de woordenlijst per artikel
 EU_bool <- newthingEU > 0L #als er meer dan 0 relevante woorden in voorkomen, is het een relevant artikel
 #in vluchtelingen_bool komt voor elk artikel te staan of dit het geval is
 
@@ -22,3 +22,5 @@ docvars(NewscorpusEU, "EU") <- EU_bool #maakt een nieuwe kolom waarin staat of h
 subset_EU <- corpus_subset(NewscorpusEU, EU == TRUE) #maakt een nieuw corpus 'tbs' met alle relevante artikelen
 
 save(subset_EU, file = "SubsetEU.RDATA") 
+
+texts(corpus_sample(subset_EU, size = 10))
