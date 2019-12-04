cba9a41d9a2dc27fd27f2bf3d46115645648557b
bdiender <57986970+bdiender@users.noreply.github.com>
2019-12-04 11:14:22 +0100

    Update splitten_op_datum.R

--- splitten_op_datum.R
+++ splitten_op_datum.R
@@ -3,7 +3,7 @@ library(quanteda)
 taug_klimaat <- corpus_subset(klimaat_volledig, Date < as.Date("2018-9-1")) #maakt een subset van het hele corpus t/m maand x
 aug_klimaat <- corpus_subset(taug_klimaat, Date >= as.Date("2018-8-1")) #haalt uit de subset alle artikelen tot maand x weg, maand x blijft over
 save(aug_klimaat, file = "C:/.../aug_klimaat.rdata") #slaat de maand op, op de puntjes komt uiteraard het filepath te staam
-
+#                                                                                                     de?
 summary(aug_klimaat$documents$Date) #checkt of de data kloppen, puur voor controle
 
 #texts(corpus_sample(mei_vlucht, size = 10)) #extra controle, haalt tien artikelen uit de subset (uit de code van Amber gehaald)
