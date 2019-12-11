cf1f37b90e87024afe0354acd245cc35f1257cf4
TechAron <57986701+TechAron@users.noreply.github.com>
2019-12-11 11:59:39 +0100

    Update sentimentanalyse.R

--- sentimentanalyse.R
+++ sentimentanalyse.R
@@ -10,3 +10,9 @@ subj_nl <- subset(lex_sentiment_nl$word, lex_sentiment_nl$subjectivity > 0)     
 sentilex <- quanteda::dictionary(list(positive=pos_sent_nl,negative=neg_sent_nl))   #maakt een dictionary met daarin de twee lijsten
 dfm_aug_kli <- quanteda::dfm(aug_klimaat)                                           #maakt een dfm van aug_klimaat
 posneg <- quanteda::dfm_lookup(dfm_aug_kli,sentilex)                                #maakt een nader te onderzoeken dfm: in deze dfm staat voor elke text hoeveel positive, negatieve, en subjectieve (pas op, deze kunnen daarnaast óók pos/neg zijn) woorden.
+posnegtbl <- tibble::as_tibble(posneg) #het maken van een tibble voor verdere behandeling
+posnegtbl$document <- NULL #zo verwijder je de kolom 'document', zodat die in de volgende stap in een keer behandeld kan worden
+
+docvars(aug_klimaat, c("Positieve woorden","Negatieve woorden","Subjectieve woorden")) <- posnegtbl #zo worden rechts in de docvars de resultaten van boven bijgeschreven. voor de <- kunnen nog extra kolommen bijgeschreven worden, zolang die ook in posneg staan.
+
+write.csv2(aug_klimaat$documents, file = "sept_EUposnegsubj.csv") #opslaan
